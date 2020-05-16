0385d053e81ce9a4bf655e5cb1a67776956fb409
SpyrosR <s.rokopanos@outlook.com>
2020-05-16 03:47:31 +0300

    Initial commit

---
+++ src/main/java/com/example/SpringBootMongoJasper/SpringBootMongoJasperApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootMongoJasper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongoJasperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongoJasperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootMongoJasper/SpringBootMongoJasperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootMongoJasper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongoJasperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
